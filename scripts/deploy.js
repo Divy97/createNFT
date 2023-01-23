85e63777a9b09cbde7e944ca251ab79f4cd0b1e9
devansh.sanghvi <devanshsanghvi.ds@gmail.com>
2023-01-23 17:48:39 +0530

    done

---
+++ scripts/deploy.js
@@ -0,0 +1,13 @@
+async function main() {
+    const MyNFT = await ethers.getContractFactory("MyNFT");
+    const myNFT = await MyNFT.deploy();
+
+    console.log("Contract deployed to address: ", myNFT.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.log(error);
+        process.exit(1)
+    })
